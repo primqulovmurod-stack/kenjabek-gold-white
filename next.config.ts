3447b9243f91652a7baf63a86eb08c9a7e9290f1
murodjonprimkulov <primqulovmurod@gmail.com>
2026-03-28 10:51:39 +0500

    feat: added xurshid-mohinur rewrite and cleanup

--- next.config.ts
+++ next.config.ts
@@ -13,6 +13,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/xurshid-mohinur/:path*',
+        destination: 'https://xurshid-mohinur-pink.vercel.app/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
